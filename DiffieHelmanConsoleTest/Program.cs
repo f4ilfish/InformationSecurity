f9445f91b22aef6cd38b16657e1453a388d9495b
f4ilfish <mishka4ernij@mail.ru>
2022-09-30 01:44:51 +0700

    Revert "<del> test proj"
    This reverts commit dac1c140b8b1e1ac117ccf560eee8e852644d5b6.

---
+++ DiffieHelmanConsoleTest/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace InformationSecurityConsoleTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Alice и Bob открыто договорились
+            int g = 2;
+            int p = 3;
+
+            Random rnd = new Random();
+
+            // Alice и Bob загадали
+            int a = rnd.Next(0, 100);
+            int b = rnd.Next(0, 100);
+
+            Console.WriteLine($"Alice a: {a}");
+            Console.WriteLine($"Bob b: {b}");
+
+            // Alice и Bob вычислили и обменялись
+            int A = (int)(Math.Pow(g, a) % p);
+            int B = (int)(Math.Pow(g, b) % p);
+
+            // Alice и Bob вычислили
+            int kAlice = (int)(Math.Pow(B, a) % p);
+            int kBob = (int)(Math.Pow(A, b) % p);
+
+            Console.WriteLine($"Alice K: {kAlice}");
+            Console.WriteLine($"Bob K: {kBob}");
+        }
+    }
+}
---
+++ InformationSecurityConsoleTest/InformationSecurityConsoleTest/Program.cs
@@ -0,0 +1,63 @@
+﻿namespace InformationSecurityConsoleTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("--Init--");
+            //var inputString = Console.ReadLine();
+
+            //var charArray = inputString.ToCharArray();
+            //var encryptedCharArray = new char[charArray.Length];
+
+            //for (int i = 0; i < charArray.Length; i++)
+            //{
+            //    encryptedCharArray[i] = Encryptor.GetCeasarEncryptedChar(charArray[i], 33);
+            //}
+
+            //Console.WriteLine("--Direct--");
+            //var outputString = new string(encryptedCharArray);
+            //Console.WriteLine(outputString);
+
+            //Console.WriteLine("--Reverse--");
+
+            //var reverseEncryptedCharArray = new char[encryptedCharArray.Length];
+            //for (int i = 0; i < encryptedCharArray.Length; i++)
+            //{
+            //    reverseEncryptedCharArray[i] = Encryptor.GetCeasarEncryptedChar(encryptedCharArray[i], 33, true);
+            //}
+            //var reverseOutputString = new string(reverseEncryptedCharArray);
+            //Console.WriteLine(reverseOutputString);
+
+            var inputString = "1234567890 ABCDIFGHIJKLMNOPQRST abcdifghijklmn АБВГДЕЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ абвгдежзиклмнопрстуфхцчшщъыьэюя";
+            var keyString = "1 12 '''12 ауауцц core";
+
+            Console.WriteLine(inputString);
+
+            //var inputCharArray = inputString.ToCharArray();
+            //var keyCharArray = keyString.ToCharArray();
+
+            //string encryptedString = string.Empty;
+            //int keyLength = keyCharArray.Length;
+            //int keyIndex = 0;
+
+            //foreach (char c in inputCharArray)
+            //{
+            //    encryptedString += CharEncryptor.GetVigenerEncryptedChar(c, keyCharArray[keyIndex]);
+            //    keyIndex++;
+            //    if (keyIndex == keyLength)
+            //    {
+            //        keyIndex = 0;
+            //    }
+            //}
+
+            //Console.WriteLine(encryptedString.ToString());
+
+            var encryptedString = StringEncryptor.GetVigenerEncyptedString(inputString, keyString, false);
+            var decryptedString = StringEncryptor.GetVigenerEncyptedString(encryptedString, keyString, true);
+
+            Console.WriteLine(encryptedString);
+            Console.WriteLine(decryptedString);
+        }
+    }
+}
